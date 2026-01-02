995599ba4eef88cf0cf7f49ac407b20339ba244f
Filipe Paula (Fsp30) <depaulacostafilipe@gmail.com>
2026-01-02 20:46:13 -0300

    fix: now command action(prettier) correcty in app

--- my-app/eslint.config.mjs
+++ my-app/eslint.config.mjs
@@ -1,18 +1,20 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
 import prettier from 'eslint-config-prettier/flat';
+import prettierPlugin from 'eslint-plugin-prettier/recommended';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
   prettier,
-  globalIgnores([
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+  prettierPlugin,
+  globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts']),
+  {
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  },
 ]);
 
 export default eslintConfig;
